5555ee5131e32f32bfc2124cc28ba3235a049513
Olli Kangasniemi <ollijk133@gmail.com>
2025-05-27 12:42:31 +0300

    read_register

--- read_values.py
+++ read_values.py
@@ -4,6 +4,7 @@ import time
 from websocket_client import WebsocketClient
 import asyncio
 from utils import extract_part
+from test import bit_high_low
 
 
 class ReadValues():
@@ -11,10 +12,10 @@ class ReadValues():
     SERVER_IP_LEFT="192.168.0.211"
     SERVER_IP_RIGHT="192.168.0.212"
     SERVER_PORT=502
-    client_leFt = ModbusTcpClient(host=SERVER_IP_LEFT, port=SERVER_PORT)
+    client_left = ModbusTcpClient(host=SERVER_IP_LEFT, port=SERVER_PORT)
     client_right = ModbusTcpClient(host=SERVER_IP_RIGHT, port=SERVER_PORT)
     client_right.connect()
-    client_leFt.connect()
+    client_left.connect()
     logger = None
     wsclient = None
 
@@ -33,7 +34,155 @@ class ReadValues():
             self.ATfile.write(f"{self.actuatortemp}\n")
             self.ICfile.write(f"{self.ic}\n")
         
-    
+    def read_register(self):
+        try:
+                registers = open("registers.txt", "w")
+
+                #Factory BoardTempTripLevel BTMP16
+                response_left = self.client_left.read_holding_registers(address=9202, count=1)
+                response_right = self.client_right.read_holding_registers(address=9202, count=1)
+                response_left_high, response_left_low = bit_high_low(response_left,5)
+                response_right_high, response_right_low = bit_high_low(response_right,5)
+                registers.write(f"""Factory BoardTempTripLevel BTMP16:
+                                \nLeft_motor:{response_left_high}.{response_left_low}
+                                \nRight_motor:{response_right_high}.{response_right_low}""")
+
+                #Factory IPEAK UCUR16
+                response_left = self.client_left.read_holding_registers(address=9204, count=1)
+                response_right = self.client_right.read_holding_registers(address=9204, count=1)
+                response_left_high, response_left_low = bit_high_low(response_left,7)
+                response_right_high, response_right_low = bit_high_low(response_right,7)
+                
+                #Factory Icontinious UCUR16
+                response_left = self.client_left.read_holding_registers(address=9205, count=1)
+                response_right = self.client_right.read_holding_registers(address=9205, count=1)
+                response_left_high, response_left_low = bit_high_low(response_left,7)
+                response_right_high, response_right_low = bit_high_low(response_right,7)
+                
+                #Factory ActuatorTempTripLevel ATMP16
+                response_left = self.client_left.read_holding_registers(address=9209, count=1)
+                response_right = self.client_right.read_holding_registers(address=9209, count=1)
+                response_left_high, response_left_low = bit_high_low(response_left,3)
+                response_right_high, response_right_low = bit_high_low(response_right,3)
+                
+                
+                #bandwidth
+                response_left = self.client_left.read_holding_registers(address=7201, count=1)
+                response_left = self.client_left.read_holding_registers(address=7231, count=1)
+
+
+
+# I peak        #IPEAK - 2560 
+                response_left = self.client_left.read_holding_registers(address=5108, count=1)
+                response_right = self.client_right.read_holding_registers(address=5108, count=1)
+
+                response_left = self.client_left.read_holding_registers(address=9205, count=1)
+                response_right = self.client_right.read_holding_registers(address=9205, count=1)
+
+                # Current operation mode
+                response_left = self.client_left.read_holding_registers(address=31, count=1)
+                response_right = self.client_right.read_holding_registers(address=31, count=1)
+
+                ### analog input parameters options
+                response_left = self.client_left.read_holding_registers(address=31, count=1)
+                response_right = self.client_right.read_holding_registers(address=31, count=1)
+
+
+                #### analog min min range
+                response_left = self.client_left.read_holding_registers(address=7218, count=2)
+                response_right = self.client_right.read_holding_registers(address=7218, count=2) 
+                registers.write(f"""Factory BoardTempTripLevel BTMP16:
+                                \nLeft_motor:{response_left.registers[0]}.{response_left.registers[1]}
+                                \nRight_motor:{response_right.registers[0]}.{response_right.registers[1]}""")
+
+
+                ### user mode 2 analog in min
+                response_left = self.client_left.read_holding_registers(address=7210, count=2)
+                response_right = self.client_right.read_holding_registers(address=7210, count=2) 
+
+                ### user mode 2 analog in max
+                response_left = self.client_left.read_holding_registers(address=7212, count=2)
+                response_right = self.client_right.read_holding_registers(address=7212, count=2) 
+
+                ### analog max range
+                response_left = self.client_left.read_holding_registers(address=7220, count=2)
+                response_right = self.client_right.read_holding_registers(address=7220, count=2) 
+
+                  ### analog min adc range
+                response_left = self.client_left.read_holding_registers(address=7214, count=2)
+                response_right = self.client_right.read_holding_registers(address=7214, count=2)
+
+                ### analog max adc range
+                ### 15900
+                response_left = self.client_left.read_holding_registers(address=7216, count=2)
+                response_right = self.client_right.read_holding_registers(address=7216, count=2) 
+
+                # current revolutions
+                response_left = self.client_left.read_holding_registers(address=378, count=2)
+                response_right = self.client_right.read_holding_registers(address=378, count=2) 
+
+                # host control command mode
+                response_left = self.client_left.read_holding_registers(address=4303, count=1)
+                response_right = self.client_right.read_holding_registers(address=4303, count=1) 
+
+                # analog i channel
+                response_left = self.client_left.read_holding_registers(address=7101, count=1)
+                response_right = self.client_right.read_holding_registers(address=7101, count=1) 
+
+                # alt command mode
+                response_left = self.client_left.read_holding_registers(address=5107, count=1)
+                response_right = self.client_right.read_holding_registers(address=5107, count=1)
+
+                 # defaul modet
+                response_left = self.client_left.read_holding_registers(address=5106, count=1)
+                response_right = self.client_right.read_holding_registers(address=5106, count=1)
+
+                # IEG MODE
+                response_left = self.client_left.read_holding_registers(address=4316, count=1) 
+                response_right = self.client_right.read_holding_registers(address=4316, count=1)
+
+                # IEG MOTION
+                response_left = self.client_left.read_holding_registers(address=4317, count=1) 
+                response_right = self.client_right.read_holding_registers(address=4317, count=1)
+
+                #ANALOG POS MIN
+                response_left = self.client_left.read_holding_registers(address=7102, count=2) 
+                response_right = self.client_right.read_holding_registers(address=7102, count=2) 
+
+                # ANALOG POS MAX
+                response_left = self.client_left.read_holding_registers(address=7104, count=2)
+                response_right = self.client_right.read_holding_registers(address=7104, count=2)
+
+                 # VELOCITY 213 - 256
+                response_left = self.client_left.read_holding_registers(address=7106, count=2)
+                response_right = self.client_right.read_holding_registers(address=7106, count=2)
+                
+                # ACCEL
+                response_left = self.client_left.read_holding_registers(address=7108, count=2) 
+                response_right = self.client_right.read_holding_registers(address=7108, count=2)
+
+                 # MODBUSCNTRL
+                response_left = self.client_left.read_holding_registers(address=7188, count=1)
+                response_right = self.client_right.read_holding_registers(address=7188, count=1)
+
+                # self.client_left.write_register(address=7188, value=3000)
+
+                # OEG sttus
+                response_right = self.client_right.read_holding_registers(address=104, count=1)
+                response_left = self.client_right.read_holding_registers(address=104, count=1)
+                test1 = bin(response_left.registers[0])
+                test2 = bin(response_right.registers[0])
+
+                # home åpsition
+                response_left = self.client_right.read_holding_registers(address=6002, count=2)
+                response_right = self.client_right.read_holding_registers(address=6002, count=2)
+
+                 # I peak factory
+                response_left = self.client_left.read_holding_registers(address=9204, count=1)
+                response_right = self.client_right.read_holding_registers(address=9204, count=1)
+        finally:
+            registers.close()
+            
     async def init(self):
         self.logger = setup_logging("read_telemetry", "read_telemetry.txt")
         self.wsclient = WebsocketClient(self.logger, on_message=self.on_message)
