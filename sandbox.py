c27ede34f735e0d39710e9d7b978ee340e8763f0
EmilFrisk1 <vichy9876@gmail.com>
2025-06-13 13:29:19 +0300

    asd

--- sandbox.py
+++ sandbox.py
@@ -113,14 +113,14 @@ class Sandbox():
             response_right = self.client_right.read_holding_registers(address=5118, count=2)
             response_left = utils.registers_convertion(response_left.registers, format="16.16", signed=False)        
             response_right = utils.registers_convertion(response_right.registers, format="16.16", signed=False)
-            self.write_to_file(file=registers_file, title="Plimit minus", left_vals=[left_definitons], right_vals=[right_definitions])
+            self.write_to_file(file=registers_file, title="Plimit minus", left_vals=[response_left], right_vals=[response_right])
 
             # Plimit plus
             response_left = self.client_left.read_holding_registers(address=5120, count=2)
             response_right = self.client_right.read_holding_registers(address=5120, count=2)
             response_left = utils.registers_convertion(response_left.registers, format="16.16", signed=False)        
             response_right = utils.registers_convertion(response_right.registers, format="16.16", signed=False)
-            self.write_to_file(file=registers_file, title="Plimit plus", left_vals=[left_definitons], right_vals=[right_definitions])
+            self.write_to_file(file=registers_file, title="Plimit plus", left_vals=[response_left], right_vals=[response_right])
 
             ### ALL PRESENT FAULTS
             response_left = self.client_left.read_holding_registers(address=5, count=1)
