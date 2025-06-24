998568463ed672358461c5240ba87dea7fd18a80
Olli Kangasniemi <ollijk133@gmail.com>
2025-06-24 15:01:03 +0300

    CrazyDemo

--- ask_telemetry.py
+++ ask_telemetry.py
@@ -1,21 +1,33 @@
 from tcp_socket_client import TCPSocketClient
 from time import sleep
 from utils import extract_part
-def recive_telemetry_data(message):
-    message=extract_part("message=", message)
-    pitch,roll = message.split(",")
-    pitch = float(pitch)
-    roll = float(roll)
-    pitch -= 0.3
-    roll -= 0.5
-    print(pitch,",",roll)
+from motionplatform_interface import MotionPlatformInterface
 
+class CrazyDemo():
+    def recive_telemetry_data(self,message):
+        message=extract_part("message=", message)
+        pitch,roll = message.split(",")
+        pitch = float(pitch)
+        roll = float(roll)
+        pitch -= 0.3
+        roll -= 0.5
+        self.mpi.set_angles(pitch,roll)
+        print(pitch,",",roll)
+        
+
+
+    def init(self):
+        self.mpi = MotionPlatformInterface()
+        self.mpi.init()
 
 if __name__ == "__main__":
-    tcp_client = TCPSocketClient(host="10.214.33.19", port=7001, on_message_received=recive_telemetry_data)
+    crazyDemo = CrazyDemo()
+    crazyDemo.init() # Initializes mpi class
+    tcp_client = TCPSocketClient(host="10.214.33.19", port=7001, on_message_received=crazyDemo.recive_telemetry_data)
     tcp_client.connect()
+
     while True:
         tcp_client.send_message("action=r_xl|")
-        sleep(1)
-    
-    
+        sleep((1/10))
+        
+        
