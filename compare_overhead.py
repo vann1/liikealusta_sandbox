01968564cd463f4c0f63dcf59e7dddcd0a39a18f
EmilFrisk1 <vichy9876@gmail.com>
2025-07-03 14:26:51 +0300

    asd

--- compare_overhead.py
+++ compare_overhead.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     file2_rolls = []
 
     for line in file1.readlines():
-        data = line.strip().split(",")
+        data = line.strip().split("|")
         file1_pitches.append(data[0])
         file1_rolls.append(data[1])
     
     for line in file2.readlines():
-        data = line.strip().split(",")
+        data = line.strip().split("|")
         file2_pitches.append(data[0])
         file2_rolls.append(data[1])
     
@@ -25,9 +25,10 @@ if __name__ == "__main__":
     diff_sum = 0
     roll_diff_sum = 0
     for i in range(100):
-        diff = abs(float(file1_pitches[i]) - float(file2_pitches[i]))
+        diff = abs(file1_pitches[i] - file2_pitches[i])
+        roll_diff = abs(file1_rolls[i] - file2_rolls[i])
         diff_sum += diff
-        roll_diff_sum += roll_diff
+        roll_diff_sum += roll_diff#
         diffs.append(diff)
         roll_diffs.append(roll_diff)
     
