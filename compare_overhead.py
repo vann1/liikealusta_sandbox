683709d6d12de49c99e4a896d6848b2562a414b7
EmilFrisk1 <vichy9876@gmail.com>
2025-07-03 14:25:09 +0300

    asdasdasd

--- compare_overhead.py
+++ compare_overhead.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     avg_diff = diff_sum/len(diffs)
     avg_roll_diff = roll_diff_sum / len(roll_diffs)
     print(f"avg pitch diff: {avg_diff} ")
-    print(f"avg roll diff: {avg_diff} ")
+    print(f"avg roll diff: {avg_roll_diff} ")
     
 
 
